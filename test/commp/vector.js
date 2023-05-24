66ce0bddc71db765d1f89cb5d075958e1942a1e8
Irakli Gozalishvili <contact@gozala.io>
2023-05-24 00:55:27 -0700

    add more tests

--- test/commp/vector.js
+++ test/commp/vector.js
@@ -1,9 +1,4 @@
 import * as FS from 'fs'
-
-// const url = new URL('./vector.csv', import.meta.url)
-// console.log(url)
-// const response = await fetch(url.href)
-// const content = await response.text()
 const content = FS.readFileSync('./test/commp/vector.csv', 'utf8')
 
 const [, ...lines] = content.trim().split('\n')
