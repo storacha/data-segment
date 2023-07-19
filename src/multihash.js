12420e16d43ec4e8df086dd557628d81108f4da2
Irakli Gozalishvili <contact@gozala.io>
2023-07-19 12:05:03 -0700

    add missing comment

--- src/multihash.js
+++ src/multihash.js
@@ -1,9 +1,15 @@
 import * as API from './api.js'
-import { IN_BYTES_PER_QUAD, FR_RATIO, MIN_PAYLOAD_SIZE } from './constant.js'
+import {
+  IN_BYTES_PER_QUAD,
+  IN_BITS_FR,
+  OUT_BITS_FR,
+  MIN_PAYLOAD_SIZE,
+} from './constant.js'
 import * as ZeroPad from './zero-comm.js'
 import * as Proof from './proof.js'
 import { split } from './piece/tree.js'
 import { pad } from './fr32.js'
+import { fromHeight as piceSizeFromHeight } from './piece/padded-size.js'
 
 export const name = /** @type {const} */ (
   'fr32-sha2-256-trunc254-padded-binary-tree'
@@ -16,20 +22,25 @@ export const code = 0x1011
  * The digest for the multihash is 33 bytes. The first byte defines the height
  * of the tree and the remaining 32 bytes are the sha-256 digest of the root
  * node.
+ *
  * @type {33}
  */
 export const size = 33
 
 /**
  * Since first byte in the digest is the tree height, the maximum height is 255.
+ *
  * @type {255}
  */
 export const MAX_HEIGHT = 255
 
 /**
- * Given that max size of the
+ * Max payload is determined by the maximum height of the tree, which is limited
+ * by the int we could store in one byte. We calculate the max piece size
+ * and derive max payload size that can would produce it after FR32 padding.
  */
-export const MAX_PAYLOAD_SIZE = 2 ** 255 * FR_RATIO
+export const MAX_PAYLOAD_SIZE =
+  (piceSizeFromHeight(MAX_HEIGHT) * BigInt(IN_BITS_FR)) / BigInt(OUT_BITS_FR)
 
 /**
  * Computes the digest of the given payload.
